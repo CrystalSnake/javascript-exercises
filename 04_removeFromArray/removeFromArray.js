fd53ea83b4967f38adf3306af9aec76183b23aeb
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2022-11-10 21:34:36 +0400

    Add 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function () {
+  arrayForCheck = arguments[0];
+  for (var i = 1; i < arguments.length; i++) {
+    for (item in arrayForCheck) {
+      if (arguments[i] === arrayForCheck[item]) {
+        arrayForCheck.splice(item, 1);
+      } else {
+        continue;
+      }
+    }
+  }
+  return arrayForCheck;
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
