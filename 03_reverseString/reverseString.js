41ac149216661368678ecf8bb248f127517b1324
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2022-11-10 20:49:53 +0400

    Add 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function (stringForReverse) {
+  splittedString = stringForReverse.split("");
+  return splittedString.reverse().join("");
 };
 
 // Do not edit below this line
