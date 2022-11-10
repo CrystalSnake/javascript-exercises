9240aebfacc7cebb92f2b275ebe53bc0adb3c07b
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2022-11-10 21:54:09 +0400

    Add 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function () {
+  if (
+    arguments[0] < 0 ||
+    arguments[1] < 0 ||
+    typeof arguments[0] != "number" ||
+    typeof arguments[1] != "number"
+  ) {
+    return "ERROR";
+  } else {
+    var min;
+    var max;
+    var sum = 0;
+    if (arguments[0] > arguments[1]) {
+      min = arguments[1];
+      max = arguments[0];
+    } else {
+      min = arguments[0];
+      max = arguments[1];
+    }
+    for (var i = min; i <= max; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
