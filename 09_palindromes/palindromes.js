3d17906884bb03fa4c543a7c97333e5c1d767498
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2022-11-17 22:12:14 +0400

    Add 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,41 @@
-const palindromes = function () {
+const palindromes = function (originString) {
+  let onlyLetterString = '';
+  for (let i = 0; i < originString.length; i++) {
+    if (
+      originString[i] === ' ' ||
+      originString[i] === ',' ||
+      originString[i] === '.' ||
+      originString[i] === '!'
+    ) {
+      continue;
+    } else {
+      onlyLetterString += originString[i];
+    }
+  }
 
+  let checkString = onlyLetterString.toLowerCase();
+  let reverseString = '';
+  let i = checkString.length - 1;
+  while (i >= 0) {
+    if (
+      checkString[i] === ' ' ||
+      checkString[i] === ',' ||
+      checkString[i] === '.' ||
+      checkString[i] === '!'
+    ) {
+      i--;
+      continue;
+    } else {
+      reverseString += checkString[i];
+      i--;
+    }
+  }
+
+  if (checkString === reverseString) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
