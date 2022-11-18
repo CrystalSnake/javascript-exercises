9f8040b13a9d12e738ce7cff555d81b30d5d58ac
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2022-11-18 22:25:07 +0400

    Add 10, 11 and 12 exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (limit) {
+  if (+limit < 1) {
+    return 'OOPS';
+  } else {
+    let firstNumber = 1;
+    let secondNumber = 1;
+    let sum = 0;
+    for (let i = 1; i < +limit - 1; i++) {
+      sum = firstNumber + secondNumber;
+      firstNumber = secondNumber;
+      secondNumber = sum;
+      sum = 0;
+    }
+    return secondNumber;
+  }
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldest = {};
+  let oldestAge = 0;
+  for (let person of people) {
+    let age;
+    if (person.yearOfDeath === undefined) {
+      let todayYear = new Date().getFullYear();
+      age = todayYear - person.yearOfBirth;
+    } else {
+      age = person.yearOfDeath - person.yearOfBirth;
+    }
 
+    if (age > oldestAge) {
+      oldestAge = age;
+      oldest = person;
+    }
+  }
+  return oldest;
 };
 
 // Do not edit below this line
